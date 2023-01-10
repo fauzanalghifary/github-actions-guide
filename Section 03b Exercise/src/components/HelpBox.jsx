bb6a6507a59db3720036842540c41023bc507490
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-01-10 11:25:22 +0700

    add two deplopyment

---
+++ Section 03b Exercise/src/components/HelpBox.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { string } from "prop-types";
+
+import "./HelpBox.css";
+
+function HelpBox({ title, text }) {
+  return (
+    <article className="help-box">
+      <h2>{title}</h2>
+      <p>{text}</p>
+    </article>
+  );
+}
+
+HelpBox.propTypes = {
+  title: string,
+  text: string,
+};
+
+export default HelpBox;
